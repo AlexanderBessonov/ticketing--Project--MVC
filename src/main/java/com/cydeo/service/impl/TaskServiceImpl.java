79a68b2420b1069a0f91d9f44522415757617101
Alexander Bessonov <bessonovUSA@gmail.com>
2022-10-07 16:32:45 -0500

    Ticketing part--5 deleteTask

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -2,10 +2,13 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -13,6 +16,16 @@ public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements
 
     @Override
     public TaskDTO save(TaskDTO task) {
+
+        if (task.getTaskStatus() == null)
+            task.setTaskStatus(Status.OPEN);
+
+        if (task.getAssignedDate() == null)
+            task.setAssignedDate(LocalDate.now());
+
+        if (task.getId() == null){
+            task.setId(UUID.randomUUID().getMostSignificantBits());
+        }
 
         return super.save(task.getId(), task);
     }
